d9caa32718056ff840c63abdac78941e58be1626
sirfromfrance <gritsenko5555@gmail.com>
2024-05-21 23:43:03 +0300

    add modal

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 document.addEventListener("DOMContentLoaded", function () {
   let slideIndex = 0;
   showSlides();
@@ -15,5 +16,55 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 1500);
+  }
+});
+
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
+const modalTitle = document.getElementById("modal-title");
+const modalContent = document.getElementById("modal-content");
+
+function openModal(platform) {
+  let title, content;
+  switch (platform) {
+    case "instagram":
+      title = "Instagram";
+      content = "Посилання на наш інстаграм:";
+      break;
+    case "pinterest":
+      title = "Pinterest";
+      content = "Посилання на наш пінтерест:";
+      break;
+    case "telegram":
+      title = "Telegram";
+      content = "Посилання на наш телеграм:";
+      break;
+  }
+  modalTitle.textContent = title;
+  modalContent.textContent = content;
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+btnsOpenModal.forEach((button) => {
+  button.addEventListener("click", () => {
+    const platform = button.getAttribute("data-platform");
+    openModal(platform);
+  });
+});
+
+overlay.addEventListener("click", closeModal);
+btnCloseModal.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
   }
 });
